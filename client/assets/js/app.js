7aea2625ffb19354e061b4e4c6931d2a060fb85b
zocke1r <zocke1r@gmail.com>
2016-03-01 13:58:38 +0100

    Ported Leagues

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -152,6 +152,7 @@ function escapeField(result) {
 		$http.get('assets/terms/uniques.yml').then(mergeIntoTermsMap);
 		$http.get('assets/terms/basetypes.yml').then(mergeIntoTermsMap);
 		$http.get('assets/terms/currencies.yml').then(mergeIntoTermsMap);
+		$http.get('assets/terms/leagues.yml').then(mergeIntoTermsMap);
 		
 		$scope.doSearch = function() {
 			$scope.Response = null;
